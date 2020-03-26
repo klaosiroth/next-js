3a9dfc07fc4d0be708324a12f2548d9d5f760eb5
klaosiroth <klaosiroth@gmail.com>
2020-03-26 10:16:48 +0700

    create about page and routing

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default () => {
+  return (
+    <div>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+      <p>This is the about page</p>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Welcome to Next.js!</div>;
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h2>Welcome to Next.js!</h2>
+    <Link href="/about">
+      <a>Go to About</a>
+    </Link>
+  </div>
+);
